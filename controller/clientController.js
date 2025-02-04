cb8fa1f9c32e42221701802cab79a9e00c856db7
David Amorim <davi.amorin2@gmail.com>
2025-02-04 20:36:38 -0300

    Criação dos metodos GET e POST

--- controller/clientController.js
+++ controller/clientController.js
@@ -1,14 +1,35 @@
 const database = require("../config/conectionDB");
-
+let TABLE = "tb_clientes";
 
 // GET
-const buscarClientes = async (request, response) =>{
-    let dados = await database.executar(`SELECT * FROM tb_clientes;`)
+const buscarClientes = async (request, response) => {
+  let dados = await database.executar(`SELECT * FROM ${TABLE};`);
 
-    response.send(dados);
-}
+  response.send(dados);
+};
 
+const buscarClientesPorId = async (request, response) => {
+  let id = request.params.id;
+
+  let dados = await database.executar(
+    `SELECT * FROM ${TABLE} WHERE id = ${id}`
+  );
+
+  response.send(dados);
+};
+
+const inserirNovoCliente = async (request, response) => {
+  let { nome, email, data_cadastro, telefone, classe } = request.body;
+
+  let dados = await database.executar(`
+        INSERT INTO ${TABLE}(nome, email, data_cadastro, telefone, classe) 
+        VALUES ('${nome}', '${email}', '${data_cadastro}', '${telefone}', '${classe}');`);
+
+  response.status(201).send({ mensage: `Cliente cadastrado com sucesso` });
+};
 
 module.exports = {
-    buscarClientes
-}
+  buscarClientes,
+  buscarClientesPorId,
+  inserirNovoCliente
+};
--- routes/clientRoute.js
+++ routes/clientRoute.js
@@ -3,5 +3,7 @@ const router     = express.Router();
 const clientController = require("../controller/clientController");
 
 router.get('/', clientController.buscarClientes);
+router.get('/:id', clientController.buscarClientesPorId);
+router.post('/', clientController.inserirNovoCliente);
 
 module.exports = router;
